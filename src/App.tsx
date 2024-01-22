080128e222a4f9ce844aa92dc544699b0b3190c7
Alexey <lexa7.03.1994@yandex.ru>
2024-01-22 21:03:38 +0700

    Dialogs 18

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import './App.css';
 import { Header } from './components/header/Header';
 import { Nav } from './components/nav/Nav';
+import { Dialogs } from './components/dialogs/Dialogs';
 import { Profile } from './components/profile/Profile';
 
 const App = () => {
@@ -9,7 +10,10 @@ const App = () => {
     <div className="App">
       <Header />
       <Nav />
-      <Profile />
+      <div className='App-wrapper-content'>
+        <Dialogs />
+        {/* <Profile /> */}
+      </div>
     </div>
   );
 }
